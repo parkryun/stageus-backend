200b1609cb6a8162e7de342ffc4f6d7f5d4ef467
parkryun <qkrfbs69@naver.com>
2022-10-22 03:51:07 +0000

    api update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express")  // 다른 js파일 연결 가능 require
-const app = express() //express 문법 사용한다고
+const express = require("express")  // 다른 js파일 연결 가능 require // 여기서는 설치된 express import
+const app = express() //express 문법 사용한다고 // import한걸 가져오는거
 const port = 3000
 
 app.get("/mainPage", (req, res) => {    // request(프론트에서 오는거 다 여기), response(백엔드에서 보내줄거) 다 오브젝트 형태로옴, 주소3000/mainpage이런거임
@@ -7,7 +7,7 @@ app.get("/mainPage", (req, res) => {    // request(프론트에서 오는거 다
     //sendFile이게 뭔뜻이었지 // 아 .3000/mainPage를 들어가면 /mainPage.html을 보내준다고
 })
 
-
+// 이렇게 경로를 적고 하는거 자체가 라우터라는건가?
 app.post("/login", (req, res) => {
 
 })
@@ -24,3 +24,6 @@ app.post("/account", (req, res) => {        //회원가입
 app.listen(port, () => {    // listen은 이 Js를 구동하면 실행하는 것 매개변수를 받고 실행
     console.log(`${port} 번에서 웹 서버가 시작됨`)
 })
+
+
+// 
