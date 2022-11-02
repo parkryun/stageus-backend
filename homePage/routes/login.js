3f71c29f55d05c044e9f611dd5de1e65d4db25d3
parkryun <qkrfbs69@naver.com>
2022-11-02 01:56:24 +0000

    create session & login session

--- homePage/routes/login.js
+++ homePage/routes/login.js
@@ -9,7 +9,18 @@ router.get("/", (req, res) => {
 
 // 아이디 비밀번호 일치여부 확인 api  넣어야하니까 Post
 router.post("/checkLogin", (req, res) => {
-    
+    // 여부확인 하고
+    const userID = req.userID
+    const userPW = req.userPW
+
+    req.session.userID = userID
+    req.session.userPW = userPW
+    req.session.isLogined = true;
+})
+
+// 로그아웃
+router.get("/logout", (req, res) => {
+    req.session.destroy()
 })
 
 
--- homePage/server.js
+++ homePage/server.js
@@ -1,6 +1,9 @@
 const express = require("express")  // 다른 js파일 연결 가능 require // 여기서는 설치된 express import
 const path = require("path")
+const session = require('express-session')
+const Memorystore = require('memorystore')(session)
 const app = express() //express 문법 사용한다고 // import한걸 가져오는거
+
 const loginRouter = require("./routes/login")
 const boardRouter = require("./routes/board")
 const postRouter = require("./routes/post")
@@ -9,11 +12,26 @@ const findRouter = require("./routes/find")
 
 const port = 3000
 
+const maxAge = 1000 * 60 * 5 // 5분 설정
+
+const sessionObj = {
+    secret: "wegf6124@#$@#!",  // 암호화를 할 때 필요한 요소값 쿠키 변조 방지
+    resave: false, // 변경사항 없어도 항상 저장할건지
+    saveUninitialized: true,    // 만들었을때 수정 안하면 uninitialized
+    store: new Memorystore({ checkPeriod: maxAge }),  // 서버를 저장할 공간 설정, checkPeriod > 서버쪽 세션의 유효기간
+    cookie: {   // 쿠키 속성 값
+        maxAge: maxAge     // 브라우저 쿠키의 유효기간
+    }
+}
+
 app.use('/login', loginRouter)
 app.use('/board', boardRouter)
 app.use('/post', postRouter)
 app.use('/join', joinRouter)
 app.use('/find', findRouter)
+
+app.use(session(sessionObj))
+app.use(express.urlencoded({extended: true}))
 
 app.get("/mainPage", (req, res) => {    // request(프론트에서 오는거 다 여기), response(백엔드에서 보내줄거) 다 오브젝트 형태로옴, 주소3000/mainpage이런거임
     res.sendFile(path.join(__dirname, "../mainPage.html"))   // js는 무조건 절대경로로 가져오는데 __dirname은 뒤에 파일 이름을 찾아서 가져옴 이게 api야 가져오는거 보내주는거
