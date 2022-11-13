05830043082c595f0d9aa3235c6571318b0b3fc3
parkryun <qkrfbs69@naver.com>
2022-11-13 08:08:16 +0000

    test

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const postApi = require("./router/post")
 const findApi = require("./router/find")
 const pagesApi = require("./router/pages")
 const accountApi = require("./router/account")
-
+const logger = require("./config/winston") // 로그
+const morgan = require("morgan") // morgan 모듈
+const combined = ':remote-addr - :remote-user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'
+const morganFormat = process.env.NODE_ENV !== "production" ? "dev" : combined
 
 const port = 3000
 
@@ -30,17 +33,21 @@ app.use(express.json())
 
 app.use('/post', postApi)
 app.use('/find', findApi)
-app.use("/", pagesApi)
-app.use("/account", accountApi)
+app.use('/', pagesApi)
+app.use('/account', accountApi)
+app.use(morgan('dev'))
 
 app.get("/mainPage", (req, res) => {    // request(프론트에서 오는거 다 여기), response(백엔드에서 보내줄거) 다 오브젝트 형태로옴, 주소3000/mainpage이런거임
     res.sendFile(path.join(__dirname, "../mainPage.html"))   // js는 무조건 절대경로로 가져오는데 __dirname은 뒤에 파일 이름을 찾아서 가져옴 이게 api야 가져오는거 보내주는거
     //sendFile이게 뭔뜻이었지 // 아 .3000/mainPage를 들어가면 /mainPage.html을 보내준다고
 })
 
-// 이렇게 경로를 적고 하는거 자체가 라우터라는건가?
-
-
+app.get("/test", (req, res) => {
+    logger.info('test')
+    res.status(200).send({
+        message: "info test"
+    })
+})
 
 app.listen(port, () => {    // listen은 이 Js를 구동하면 실행하는 것 매개변수를 받고 실행
     console.log(`${port} 번에서 웹 서버가 시작됨`)
