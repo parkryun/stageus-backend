3e82c89afc26589ccd9109d43f9c8a8e2c4e003a
parkryun <qkrfbs69@naver.com>
2022-11-22 19:01:53 +0000

    complete upload imgage function

--- module/multer.js
+++ module/multer.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 const multerS3 = require('multer-s3')
 const aws = require('aws-sdk')
 const s3Option = require('../config/s3')
-const dateTime = require("./date") // date
+const moment = require("moment")
 
 const s3 = new aws.S3(s3Option)
 
@@ -12,7 +12,8 @@ const storage = multerS3({
     bucket: "stageus-ryun",
     contentType: multerS3.AUTO_CONTENT_TYPE, 
     key: (req, file, callback) => {
-        callback(null, `${dateTime}_${file.originalname}`)
+        let datetime = moment().format('YYYYMMDDHHmmss')
+        callback(null, `${datetime}_${file.originalname}`)
     }
 })
 
